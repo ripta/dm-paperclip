ad40d00b51d14de6ea07ccde82ddbd6b1b7cbe60
Markus Schirp <mbj@seonic.net>
2010-08-09 13:18:24 +0800

    fixed a tmpfile race issue

--- lib/dm-paperclip/storage.rb
+++ lib/dm-paperclip/storage.rb
@@ -36,10 +36,9 @@ module Paperclip
 
       def flush_writes #:nodoc:
         @queued_for_write.each do |style_name, file|
-          file.close
           FileUtils.mkdir_p(File.dirname(path(style_name)))
           log("saving #{path(style_name)}")
-          FileUtils.mv(file.path, path(style_name))
+          FileUtils.cp(file.path, path(style_name))
           FileUtils.chmod(0644, path(style_name))
         end
         @queued_for_write = {}
